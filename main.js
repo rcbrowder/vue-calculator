ea4cbd410aa1c30722e687339a31b45d3d8fc62f
Chris Browder <rcbrowder@gmail.com>
2018-06-27 22:22:40 -0400

    Basic functions working. Need to implement decimal and tape register display.

--- main.js
+++ main.js
@@ -1,6 +1,66 @@
 var main = new Vue({
     el: '#main',
     data: {
-        display: 0
+        currentValue: 0,
+        previousValue: 0,
+        operator: '',
+        tapeRegister: []
+    },
+    methods: {
+
+        numericButtonPress(digit) {
+            if (this.operator == '=') {
+                this.previousValue = 0;
+            }
+            this.currentValue = this.currentValue * 10 + digit;
+        },
+
+        operatorButtonPress(operatorKey) {
+            if (this.operator != '') {
+                this.operate();
+            }
+            else {
+                this.previousValue = this.currentValue;
+            }
+            this.currentValue = 0;
+            this.operator = operatorKey;
+        },
+
+
+        operate() {
+            if (this.operator == '+') {
+                this.previousValue += this.currentValue;
+            }
+            else if (this.operator == '-') {
+                this.previousValue -= this.currentValue;
+            }
+            else if (this.operator == '*') {
+                this.previousValue *= this.currentValue;
+            }
+            else if (this.operator == '/') {
+                this.previousValue /= this.currentValue;
+            }
+            else if (this.operator == '=') {
+                this.previousValue = this.currentValue;
+            }
+
+            this.currentValue = 0;
+            this.operator = '';
+        },
+
+        clear: function() {
+            this.previousValue = 0;
+            this.currentValue = 0;
+            this.operator = ''
+        }
+    },
+
+    computed: {
+        displayValue() {
+            return this.previousValue ? 
+        this.currentValue ?
+        this.currentValue : this.previousValue
+        : this.currentValue
+        }
     }
 });
